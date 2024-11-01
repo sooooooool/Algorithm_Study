c77c08b3ce8aea116d5fb83adcfa8aa0c74de700
sooooooool <awesomena89@gmail.com>
2024-11-01 09:49:12 +0900

    [level 0] Title: 접미사 배열, Time: 0.21 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.js
@@ -0,0 +1,8 @@
+function solution(my_string) {
+    const suffixes = [];
+    for (let i = 0; i < my_string.length; i ++) {
+        suffixes.push(my_string.slice(i));
+    } 
+    suffixes.sort();
+    return suffixes;
+}
